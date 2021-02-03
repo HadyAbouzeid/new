0f79a3a35e638490fe2a984b5416521fd78ff431
Hady Abouzeid <44134810+HadyAbouzeid@users.noreply.github.com>
2021-02-03 17:11:01 +0100

    Add files via upload

---
+++ Drag.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drag : MonoBehaviour
+{
+    private bool isDragging;
+
+    
+    void OnMouseDown()
+    {
+        isDragging = true;
+
+    }
+
+    void OnMouseUp()
+    {
+        isDragging = false;
+    }
+
+    void Update()
+    {
+        if (isDragging)
+        {
+            Vector2 mousePosition = Camera.main. ScreenToWorldPoint(Input.mousePosition) - transform.position;
+            transform.Translate(mousePosition);
+        }
+    }
+}
